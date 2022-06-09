3910582908bb129bd4684baa9de3254d7f33df52
ya-nsh <yansh2017@gmail.com>
2022-06-09 23:35:30 +0530

    added basic sign-in page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Home from './routes/home/home.component';
 import { Route, Routes, Outlet } from 'react-router-dom';
 
 import Navigation from './routes/navigation/navigation.component';
+import SignIn from './routes/sign-in/sign-in.component';
 
 const Shop = () => {
   return <h1>Shop</h1>;
@@ -13,6 +14,7 @@ function App() {
       <Route path="/" element={<Navigation />}>
         <Route index={true} element={<Home />} />
         <Route path="shop" element={<Shop />} />
+        <Route path="sign-in" element={<SignIn />} />
       </Route>
     </Routes>
   );
