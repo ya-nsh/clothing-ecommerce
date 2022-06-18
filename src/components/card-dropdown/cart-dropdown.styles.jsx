8b73f308f5ab2b28eb1c75e480e86b0815678c42
ya-nsh <yansh2017@gmail.com>
2022-06-18 23:46:28 +0530

    fixed button margin in cart-dropdown

--- src/components/card-dropdown/cart-dropdown.styles.jsx
+++ src/components/card-dropdown/cart-dropdown.styles.jsx
@@ -1,6 +1,10 @@
 import styled from 'styled-components';
 
-import Button from '../button/button.component';
+import {
+  BaseButton,
+  GoogleSignInButton,
+  InvertedButton
+} from '../button/button.styles';
 
 export const CartDropdownContainer = styled.div`
   position: absolute;
@@ -14,7 +18,9 @@ export const CartDropdownContainer = styled.div`
   top: 90px;
   right: 40px;
   z-index: 5;
-  ${Button} {
+  ${BaseButton},
+  ${GoogleSignInButton},
+  ${InvertedButton} {
     margin-top: auto;
   }
 `;
@@ -30,28 +36,3 @@ export const CartItems = styled.div`
   flex-direction: column;
   overflow: scroll;
 `;
-
-// .cart-dropdown-container {
-//   position: absolute;
-//   width: 240px;
-//   height: 340px;
-//   display: flex;
-//   flex-direction: column;
-//   padding: 20px;
-//   border: 1px solid black;
-//   background-color: white;
-//   top: 90px;
-//   right: 40px;
-//   z-index: 5;
-
-//   .cart-items {
-//     height: 240px;
-//     display: flex;
-//     flex-direction: column;
-//     overflow: scroll;
-//   }
-
-//   button {
-//     margin-top: auto;
-//   }
-// }
