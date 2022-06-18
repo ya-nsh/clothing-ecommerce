3e8991520c6f4d2b1a00e9fc3a85158cf330d580
ya-nsh <yansh2017@gmail.com>
2022-06-18 23:35:22 +0530

    added directory routing

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,41 +1,14 @@
+import { Outlet } from 'react-router-dom';
+
 import Directory from '../../components/directory/directory.component';
 
-function Home() {
-  const categories = [
-    {
-      id: 1,
-      title: 'hats',
-      imageUrl:
-        'https://images.unsplash.com/photo-1576871337632-b9aef4c17ab9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
-    },
-    {
-      id: 2,
-      title: 'jackets',
-      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png'
-    },
-    {
-      id: 3,
-      title: 'sneakers',
-      imageUrl:
-        'https://images.unsplash.com/photo-1552346154-21d32810aba3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
-    },
-    {
-      id: 4,
-      title: 'womens',
-      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png'
-    },
-    {
-      id: 5,
-      title: 'mens',
-      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png'
-    }
-  ];
-
+const Home = () => {
   return (
-    <>
-      <Directory categories={categories} />
-    </>
+    <div>
+      <Directory />
+      <Outlet />
+    </div>
   );
-}
+};
 
 export default Home;
