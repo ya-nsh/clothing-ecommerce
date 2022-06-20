3efa455d3322de9e2ca2f15c8487e652638fa2ff
ya-nsh <yansh2017@gmail.com>
2022-06-20 17:25:00 +0530

    refactored useState to useReducer

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,4 +1,4 @@
-import { useState, createContext, useEffect } from 'react';
+import { createContext, useEffect, useReducer } from 'react';
 
 import {
   onAuthStateChangedListener,
@@ -10,8 +10,39 @@ export const UserContext = createContext({
   setCurrentUser: () => null
 });
 
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: 'SET_CURRENT_USER'
+};
+
+const userReducer = (state, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: payload
+      };
+    default:
+      return state;
+  }
+};
+
 export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
+  // const [currentUser, setCurrentUser] = useState(null);
+
+  const INITIAL_STATE = {
+    currentUser: null
+  };
+
+  const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE);
+
+  const setCurrentUser = user => {
+    dispatch({
+      type: USER_ACTION_TYPES.SET_CURRENT_USER,
+      payload: user
+    });
+  };
 
   const value = { currentUser, setCurrentUser };
 
